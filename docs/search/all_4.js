dfcbc17949a448aabba2676657722f973f0f5174
akerumort <akerumort@gmail.com>
2024-06-03 19:45:42 +0300

    Added all files with docs and setup

---
+++ docs/search/all_4.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['main_0',['Main',['../class_calculator_1_1_app.html#a794cb0c05145d9ce160bb029cd13e88d',1,'Calculator.App.Main()'],['../class_calculator_1_1_app.html#a794cb0c05145d9ce160bb029cd13e88d',1,'Calculator.App.Main()']]],
+  ['mainwindow_1',['MainWindow',['../class_calculator_1_1_main_window.html',1,'Calculator.MainWindow'],['../class_calculator_1_1_main_window.html#ada9d498d66aba920a1c1d9fe1eeb7a01',1,'Calculator.MainWindow.MainWindow()']]],
+  ['mainwindow_2eg_2ecs_2',['MainWindow.g.cs',['../_main_window_8g_8cs.html',1,'']]],
+  ['mainwindow_2eg_2ei_2ecs_3',['MainWindow.g.i.cs',['../_main_window_8g_8i_8cs.html',1,'']]],
+  ['mainwindow_2examl_2ecs_4',['MainWindow.xaml.cs',['../_main_window_8xaml_8cs.html',1,'']]]
+];
