199744c242ef2d136a3547b9bb40140e0dbcfefe
Ivan Rodionov <144949516+akerumort@users.noreply.github.com>
2024-09-23 15:37:45 +0300

    fixed .gitignore

---
+++ docs/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['calculator_0',['Calculator',['../namespace_calculator.html',1,'']]]
+];
---
+++ docs/search/functions_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['main_0',['Main',['../class_calculator_1_1_app.html#a794cb0c05145d9ce160bb029cd13e88d',1,'Calculator.App.Main()'],['../class_calculator_1_1_app.html#a794cb0c05145d9ce160bb029cd13e88d',1,'Calculator.App.Main()']]],
+  ['mainwindow_1',['MainWindow',['../class_calculator_1_1_main_window.html#ada9d498d66aba920a1c1d9fe1eeb7a01',1,'Calculator::MainWindow']]]
+];
